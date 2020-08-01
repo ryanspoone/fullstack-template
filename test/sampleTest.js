334757dfdebb386818e224b83706f8a546bba251
Ryan Spoone <ryanspoone@gmail.com>
2020-07-31 22:41:33 -0500

    Add sample test

---
+++ test/sampleTest.js
@@ -0,0 +1,11 @@
+/**
+ * @file Sample test file. Remove once adding tests.
+ */
+
+import assert from 'assert';
+
+describe('Sample test', () => {
+    it('should pass', () => {
+        assert(true);
+    });
+});
