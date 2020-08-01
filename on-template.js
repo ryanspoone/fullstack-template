22ef394f898708a1e57febd7c60be4cbcf53e9f1
Ryan Spoone <ryanspoone@gmail.com>
2020-07-31 23:04:03 -0500

    Improve template replacements

--- on-template.js
+++ on-template.js
@@ -1,16 +1,24 @@
 #!/usr/bin/env node
 
 import { writeFileSync, readFileSync, unlinkSync } from 'fs';
-
-import packageJson from './package.json';
+import _ from 'lodash';
+import rootPackage from './package.json';
+import clientPackage from './client/package.json';
+import serverPackage from './server/package.json';
 
 const TEMPLATE_AUTHOR = 'Ryan Spoone';
 const TEMPLATE_GITHUB_REPOSITORY = 'ryanspoone/fullstack-template';
 const { GITHUB_REPOSITORY, GITHUB_ACTOR } = process.env;
+const TEMPLATE_REPO_NAME = _(TEMPLATE_GITHUB_REPOSITORY)
+    .split('/')
+    .last();
+const GITHUB_REPO_NAME = _(GITHUB_REPOSITORY)
+    .split('/')
+    .last();
 
 if (GITHUB_REPOSITORY === TEMPLATE_GITHUB_REPOSITORY) {
     // eslint-disable-next-line no-console
-    console.info(`Not running inside ${TEMPLATE_GITHUB_REPOSITORY} repo.`);
+    console.error(`Not running inside ${TEMPLATE_GITHUB_REPOSITORY} repo.`);
     process.exit();
 }
 
@@ -21,17 +29,30 @@ if (!GITHUB_REPOSITORY) {
 // eslint-disable-next-line no-console
 console.log(`${GITHUB_ACTOR}'s ${GITHUB_REPOSITORY}`);
 
-const TEMPLATE_PACKAGE_NAME = packageJson.name;
 const PACKAGE_NAME = `@${GITHUB_REPOSITORY.toLowerCase()}`;
 
 /**
  * package.json
  */
 
-packageJson.name = PACKAGE_NAME;
-packageJson.homepage = packageJson.homepage.replace(TEMPLATE_GITHUB_REPOSITORY, GITHUB_REPOSITORY);
-packageJson.author = packageJson.author.replace(TEMPLATE_AUTHOR, GITHUB_ACTOR);
-writeFileSync('./package.json', JSON.stringify(packageJson, null, 4), { encoding: 'utf8' });
+rootPackage.name = PACKAGE_NAME;
+rootPackage.homepage = rootPackage.homepage.replace(TEMPLATE_GITHUB_REPOSITORY, GITHUB_REPOSITORY);
+rootPackage.author = rootPackage.author.replace(TEMPLATE_AUTHOR, GITHUB_ACTOR);
+writeFileSync('./package.json', JSON.stringify(rootPackage, null, 4), { encoding: 'utf8' });
+
+/**
+ * client/package.json
+ */
+
+clientPackage.name = clientPackage.name.replace(TEMPLATE_REPO_NAME, PACKAGE_NAME);
+writeFileSync('./client/package.json', JSON.stringify(clientPackage, null, 4), { encoding: 'utf8' });
+
+/**
+ * server/package.json
+ */
+
+serverPackage.name = serverPackage.name.replace(TEMPLATE_REPO_NAME, PACKAGE_NAME);
+writeFileSync('./server/package.json', JSON.stringify(serverPackage, null, 4), { encoding: 'utf8' });
 
 /**
  * README.md
@@ -39,10 +60,10 @@ writeFileSync('./package.json', JSON.stringify(packageJson, null, 4), { encoding
 
 const readme = readFileSync('./README.md', { encoding: 'utf8' });
 const newReadme = readme
-    .split(TEMPLATE_PACKAGE_NAME)
-    .join(PACKAGE_NAME)
     .split(TEMPLATE_GITHUB_REPOSITORY)
-    .join(GITHUB_REPOSITORY);
+    .join(GITHUB_REPOSITORY)
+    .split(TEMPLATE_REPO_NAME)
+    .join(GITHUB_REPO_NAME);
 
 writeFileSync('./README.md', newReadme, { encoding: 'utf8' });
 
@@ -58,5 +79,5 @@ writeFileSync('./LICENSE', newLicense, { encoding: 'utf8' });
 /**
  * CLEAN UP
  */
-unlinkSync('./on-template.js');
 unlinkSync('./.github/workflows/on-template.yml');
+unlinkSync('./on-template.js');
